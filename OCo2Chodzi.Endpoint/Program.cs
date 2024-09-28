42294ff8df01e8e85bea11354cd05296d96c20d1
maj00r <januszj511@gmail.com>
2024-09-29 01:28:53 +0200

    allow any in cors

--- OCo2Chodzi.Endpoint/Program.cs
+++ OCo2Chodzi.Endpoint/Program.cs
@@ -6,11 +6,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.RegisterServiceModule(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
