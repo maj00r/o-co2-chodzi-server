0cd6dc289dd6b53fb08478f62777cf4abba86669
maj00r <januszj511@gmail.com>
2024-09-28 17:44:49 +0200

    absorbion entity typo fix

---
+++ OCo2Chodzi.Endpoint/Migrations/20240928154215_AbsorbionsEntityTypoFix.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OCo2Chodzi.Endpoint.Migrations
+{
+    /// <inheritdoc />
+    public partial class AbsorbionsEntityTypoFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty");
+
+            migrationBuilder.RenameTable(
+                name: "MyProperty",
+                newName: "Absorbions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Absorbions",
+                table: "Absorbions",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Absorbions",
+                table: "Absorbions");
+
+            migrationBuilder.RenameTable(
+                name: "Absorbions",
+                newName: "MyProperty");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty",
+                column: "Id");
+        }
+    }
+}
--- OCo2Chodzi.Service/Infrastructure/ApplicationDbContext.cs
+++ OCo2Chodzi.Service/Infrastructure/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ namespace OCo2Chodzi.Service.Infrastructure;
 
 public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
 {
-    public DbSet<Absorbion> MyProperty { get; set; }
+    public DbSet<Absorbion> Absorbions { get; set; }
     public DbSet<LinearEmission> LinearEmissions { get; set; }
     public DbSet<MassEmission> MassEmissions { get; set; }
     public DbSet<SingularEmission> SingularEmissions { get; set; }
